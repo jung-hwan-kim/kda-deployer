14725392900582d1612f4d75e812830676022d33
jungflykim <jay.kim@openlane.com>
2019-12-22 17:57:53 -0500

    init

--- src/kda_deployer/aws.clj
+++ src/kda_deployer/aws.clj
@@ -6,9 +6,23 @@
   (let [r (:out (sh "aws" "kinesisanalyticsv2" "describe-application" "--application-name" name))]
     (json/parse-string r true)))
 
+(defn kda-describe-application2[name]
+  (let [input {:ApplicationName name}
+        r (:out (sh "aws" "kinesisanalyticsv2" "describe-application" "--cli-input-json" (json/generate-string input)))]
+    (json/parse-string r true)))
+
 
 (defn create-update-request[name version objectVersion]
   {:ApplicationName name
    :CurrentApplicationVersionId version
-   :ApplicationConfigurationUpdate {:ApplicationCodeConfigurationUpdate {:CodeContentTypeUpdate "ZIPFILE"
-                                                                         :CodeContentUpdate {:S3ContentLocationUpdate {:ObjectVersionUpdate objectVersion}}}}})
+   :ApplicationConfigurationUpdate {:ApplicationCodeConfigurationUpdate {:CodeContentUpdate {:S3ContentLocationUpdate {:ObjectVersionUpdate objectVersion}}}}})
+
+(defn kda-update-application[name version objectVersion]
+  (let [input (create-update-request name version objectVersion)
+        r (:out (sh "aws" "kinesisanalyticsv2" "update-application" "--cli-input-json" (json/generate-string input)))]
+    (json/parse-string r true)))
+
+
+
+(defn s3-head-object[bucket path]
+  (json/parse-string (:out (sh "aws" "s3api" "head-object" "--bucket" bucket "--key" path)) true))
--- src/kda_deployer/core.clj
+++ src/kda_deployer/core.clj
@@ -2,15 +2,38 @@
   (:use [clojure.java.shell])
   (:require [kda-deployer.aws :as aws]))
 
-(defn build-kda-processor[]
+(defn git-pull [dir]
+  (println "git pull:" dir)
+  (let [r (:out (sh "git" "pull" :dir dir))]
+    (println r)
+    r))
+
+(defn mvn-build [dir]
+  (println "mvn-build:" dir)
+  (let [result (sh "mvn" "clean" "package" :dir dir)]
+    (println (:out result))
+    (:exit result)))
+
+(defn deploy-kda-job-jar[]
+  (println (sh "aws" "s3" "cp" "target/kda-job-1.0.jar" "s3://ds-kda/kda-job/target/kda-job-1.0.jar" :dir "../kda-job"))
   )
+
+(defn update-kda-job[]
+  (let [appVersion (:ApplicationVersionId (:ApplicationDetail (aws/kda-describe-application "ds-kda")))
+        objectVersion (:VersionId (aws/s3-head-object "ds-kda" "kda-job/target/kda-job-1.0.jar"))
+        result (aws/kda-update-application "ds-kda" appVersion objectVersion)]))
+
+(defn build-kda-processor[])
 
 (defn build-kda-job[]
   (println "Building kda-job")
-  (let [result (sh "mvn" "clean" "package" :dir "../kda-job")]
-    (println (:exit result))
+  (let [kda-job-dir "../kda-job"
+        git-pull-result git-pull kda-job-dir
+        mvn-build-result mvn-build kda-job-dir
+        ]
     (println (sh "ls" "-lh" "../kda-job/target/kda-job-1.0.jar"))
     ;;(println (:out result))
+
     (println (sh "aws" "s3" "cp" "target/kda-job-1.0.jar" "s3://ds-kda/kda-job/target/kda-job-1.0.jar" :dir "../kda-job"))
     (println (sh "aws" "s3" "ls" "ds-kda/kda-job/target/kda-job-1.0.jar"))
     )
