24a44d4ccba60eb47bfb1cbe0f97ecaa4932f9af
jungflykim <jay.kim@openlane.com>
2019-12-22 22:05:19 -0500

    init

--- src/kda_deployer/core.clj
+++ src/kda_deployer/core.clj
@@ -14,30 +14,52 @@
     (println (:out result))
     (:exit result)))
 
-(defn deploy-kda-job-jar[]
-  (println (sh "aws" "s3" "cp" "target/kda-job-1.0.jar" "s3://ds-kda/kda-job/target/kda-job-1.0.jar" :dir "../kda-job"))
-  )
+(defn upload-kda-job-jar [jar-path]
+  (println "Upload " jar-path " to S3")
+  (let [s3-path (str "s3://ds-kda/kda-job/" jar-path)
+        result (sh "aws" "s3" "cp" jar-path s3-path :dir "../kda-job")
+        ]
+    (println (:out result))
+    (if (= (:exit result) 0)
+      (let [s3-desc (aws/s3-head-object "ds-kda" (str "kda-job/" jar-path))
+            versionId (:VersionId s3-desc)]
+        (println "Uploaded " versionId)
+        versionId)
+      (println "Failed"))))
 
 (defn update-kda-job[]
   (let [appVersion (:ApplicationVersionId (:ApplicationDetail (aws/kda-describe-application "ds-kda")))
         objectVersion (:VersionId (aws/s3-head-object "ds-kda" "kda-job/target/kda-job-1.0.jar"))
         result (aws/kda-update-application "ds-kda" appVersion objectVersion)]))
 
+
+(defn deploy-kda-job[]
+  (println "deploying..")
+  (let [appVersion (:ApplicationVersionId (:ApplicationDetail (kda-deployer.aws/kda-describe-application "ds-kda")))
+        objectVersion (upload-kda-job-jar "target/kda-job-1.0.jar")
+        ]
+    (println "deploying kda - appVer:" appVersion " objectVer:" objectVersion)
+    (aws/kda-update-application "ds-kda" appVersion objectVersion)))
+
 (defn build-kda-processor[])
 
 (defn build-kda-job[]
   (println "Building kda-job")
   (let [kda-job-dir "../kda-job"
-        git-pull-result git-pull kda-job-dir
-        mvn-build-result mvn-build kda-job-dir
+        git-pull-result (git-pull kda-job-dir)
+        mvn-build-result (mvn-build kda-job-dir)
         ]
-    (println (sh "ls" "-lh" "../kda-job/target/kda-job-1.0.jar"))
+    (if (= mvn-build-result 0)
+      (let [result deploy-kda-job]
+        (println result))
+      (println "BUILD FAILED"))))
+
+    ;;(println (sh "ls" "-lh" "../kda-job/target/kda-job-1.0.jar"))
     ;;(println (:out result))
 
-    (println (sh "aws" "s3" "cp" "target/kda-job-1.0.jar" "s3://ds-kda/kda-job/target/kda-job-1.0.jar" :dir "../kda-job"))
-    (println (sh "aws" "s3" "ls" "ds-kda/kda-job/target/kda-job-1.0.jar"))
-    )
-  )
+    ;;(println (sh "aws" "s3" "cp" "target/kda-job-1.0.jar" "s3://ds-kda/kda-job/target/kda-job-1.0.jar" :dir "../kda-job"))
+    ;;(println (sh "aws" "s3" "ls" "ds-kda/kda-job/target/kda-job-1.0.jar"))
+
 
 (defn -main
   "Deploying kda main"
