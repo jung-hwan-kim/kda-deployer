7821c10c18c966e42129eae4106f4f43d8887d8d
jungflykim <jay.kim@openlane.com>
2020-01-10 18:45:24 -0500

    prototype

--- src/kda_deployer/main.clj
+++ src/kda_deployer/main.clj
@@ -17,18 +17,18 @@
 
 (defn mvn-build [dir]
   (println "mvn-build:" dir)
-  (let [result (sh "mvn" "clean" "compile" "clojure:compile" "package" :dir dir)]
+  (let [result (sh "mvn" "clean" "compile" "package" :dir dir)]
     (println (:out result))
     (if-not (= (:exit result) 0) (throw (:error result)))))
 
-(defn upload-kda-job-jar [jar-path]
+(defn upload-kda-job-jar [app-name jar-path]
   (println "Upload " jar-path " to S3")
-  (let [s3-path (str "s3://ds-kda/kda-job/" jar-path)
-        result (sh "aws" "s3" "cp" jar-path s3-path :dir "../kda-job")
+  (let [s3-path (str "s3://ds-kda/artifacts/" jar-path)
+        result (sh "aws" "s3" "cp" jar-path s3-path :dir (str "../" app-name))
         ]
     (println (:out result))
     (if (= (:exit result) 0)
-      (let [s3-desc (aws/s3-head-object "ds-kda" (str "kda-job/" jar-path))
+      (let [s3-desc (aws/s3-head-object "ds-kda" (str "artifacts/" jar-path))
             versionId (:VersionId s3-desc)]
         (println "Uploaded " versionId)
         versionId)
@@ -39,21 +39,19 @@
         objectVersion (:VersionId (aws/s3-head-object "ds-kda" "kda-job/target/kda-job-1.0.jar"))
         result (aws/kda-update-application "ds-kda" appVersion objectVersion)]))
 
-(defn deploy-and-run-in-local[]
-  ; ~/flink/bin/flink run target/kda-job-1.0.jar
-  (println "* Running in local flink..")
+(defn run-in-local[app-name]
+  (println "* Running in local flink.." app-name)
   (let [cmd (str (System/getProperty "user.home") "/flink/bin/flink")
-        result (sh cmd "run" "../kda-job/target/kda-job-1.0.jar")]
+        result (sh cmd "run" (str "../" app-name "/target/" app-name "-1.0.jar"))]
     (println (:out result))
     (if-not (= (:exit result) 0) (throw (:error result)))))
 
-(defn deploy-job-to-aws[]
-  (println "*** Deploying to AWS")
-  (let [appVersion (:ApplicationVersionId (:ApplicationDetail (kda-deployer.aws/kda-describe-application "ds-kda")))
-        objectVersion (upload-kda-job-jar "target/kda-job-1.0.jar")
-        ]
+(defn deploy-job-to-aws[app-name]
+  (println "*** Deploying to AWS" app-name)
+  (let [appVersion (:ApplicationVersionId (:ApplicationDetail (kda-deployer.aws/kda-describe-application app-name)))
+        objectVersion (upload-kda-job-jar app-name (str "target/" app-name "-1.0.jar"))]
     (println "deploying kda - appVer:" appVersion " objectVer:" objectVersion)
-    (aws/kda-update-application "ds-kda" appVersion objectVersion)))
+    (aws/kda-update-application app-name appVersion objectVersion)))
 
 (defn build-task[]
   (println "Building kda-task")
@@ -61,9 +59,9 @@
     (git-pull kda-task-dir)
     (lein-build kda-task-dir)))
 
-(defn build-job[]
-  (println "Building kda-job")
-  (let [kda-job-dir "../kda-job"]
+(defn build-job[app-name]
+  (println "Building " app-name)
+  (let [kda-job-dir (str "../" app-name)]
      (git-pull kda-job-dir)
      (mvn-build kda-job-dir)))
 
@@ -73,34 +71,39 @@
     ;;(println (sh "aws" "s3" "cp" "target/kda-job-1.0.jar" "s3://ds-kda/kda-job/target/kda-job-1.0.jar" :dir "../kda-job"))
     ;;(println (sh "aws" "s3" "ls" "ds-kda/kda-job/target/kda-job-1.0.jar"))
 
-(defn do-build[args]
+(defn do-build[app-name]
   (println "[BUILD]")
   (build-task)
-  (build-job)
+  (build-job app-name)
   (shutdown-agents)
   )
 
-(defn do-deploy-local[args]
+(defn do-deploy-local[app-name]
   (println "[DEPLOY LOCAL]")
   (build-task)
-  (build-job)
-  (deploy-and-run-in-local)
+  (build-job app-name)
+  (run-in-local app-name)
   (shutdown-agents)
   )
-(defn do-deploy-aws[args]
+(defn do-deploy-aws[app-name]
   (println "[DEPLOY AWS")
   (build-task)
-  (build-job)
+  (build-job app-name)
   (deploy-job-to-aws)
   (shutdown-agents)
+  )
+(defn do-stuff[app-name]
+  (println app-name)
+  (println (str "target/" app-name ".jar"))
   )
 (defn -main
   "Deploying kda main"
   [command & args]
   (case command
-    "build" (do-build args)
-    "deploy-local" (do-deploy-local args)
-    "deploy-aws" (do-deploy-aws args)
+    "build" (do-build (or (first args) "ds-kda-job"))
+    "deploy-local" (do-deploy-local (or (first args) "ds-kda-job"))
+    "deploy-aws" (do-deploy-aws (or (first args) "ds-kda-job"))
+    "test" (do-stuff  (or (first args) "ds-kda-job"))
     (println "Build & Deploy - options: deploy-local, deploy-aws, .. exiting without doing anything.")
     )
   )
